2cdfcd6f279c2bdd753c11cb7e4487875d82e77d
hsotosil <146412146+hsotosil@users.noreply.github.com>
2023-09-28 23:09:52 -0700

    Session 1 prompt for Coding Journal

---
+++ cj1-1.py
@@ -0,0 +1,11 @@
+def print_Hennessey():
+    print('Hennessey')
+    print('She/he/they')
+    print('My favorite movie is \"Your Name\"')
+    print('My favorite food is esquites because I recently had one and it was sooo yummy.')
+
+def main():
+    print_Hennessey()
+
+if __name__=="__main__":
+    main()
