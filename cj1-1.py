4f4fcf2c301c5eb2a96605d8a52327e140659901
hsotosil <146412146+hsotosil@users.noreply.github.com>
2023-09-28 22:44:04 -0700

    cj1-1.py

---
+++ cj1-1.py
@@ -0,0 +1,11 @@
+def print_Hennessey():
+    print('Hennessey')
+    print('She/he/they')
+    print('I have a silly dog named Tequila')
+    print('Hennessey and Tequila, what an interesting combo...')
+
+def main():
+    print_Hennessey()
+
+if __name__=="__main__":
+    main()
